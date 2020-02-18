a5d07a32d9026e011b19a510e574d70ed0ed16fc
WendyZW <ivy.valentin@hotmail.fr>
2020-02-18 16:06:52 +0100

    feat(mock):mock weather created

--- src/mocks/mockWeather.js
+++ src/mocks/mockWeather.js
@@ -1,4 +1,4 @@
-export const mockWeather {"coord":{"lon":-0.13,"lat":51.51},
+export const mockWeather = {"coord":{"lon":-0.13,"lat":51.51},
 "weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],
 "base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},
 "visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},
