33d68aedcbd8401887efcf31899e869fe6cf395f
WendyZW <ivy.valentin@hotmail.fr>
2020-02-18 17:01:06 +0100

    feat(weather):add icon,description,temperature

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Weather from './components/Weather';
 
 function App() {
   return (
-    <div className="App">
-    <Weather/>
+    <div className="App">    
       <header className="App-header">
+        <Weather/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>        
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { mockWeather } from '../mocks/mockWeather';
+import React, { useState } from 'react';
+import {mockWeather} from '../mocks/mockWeather.js'
 
 function Weather() {
+
+    const[weather, setWeather] = useState(mockWeather);
+    
+    function KelvinToCelsius(tempKelvin){
+        return Math.round(tempKelvin - 273.15);
+    }
+
+    function loadIconWeather(idIcon) {
+        return "http://openweathermap.org/img/wn/"+idIconn+"@2x.png";
+    }
+
     return (
         <div>
-            <h1>Meteo</h1>
-
+            <h1>Météo : {weather.name}</h1>
+            <img src={loadIconCelcius(weather.weather[0].icon)} alt="pluie" />
+            <p>{weather.weather[0].description}</p>
+            <p>{KelvinToCelsius(weather.main.temp)}°C</p>
         </div>
     )
 }
 
-export default Weather
+export default Weather;
