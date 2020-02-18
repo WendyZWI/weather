b4a6b29920a693581c1421b38e1ff7fa3d1f3c7f
WendyZW <ivy.valentin@hotmail.fr>
2020-02-18 15:19:00 +0100

    feat(global):initialization of my weather app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <div className="App">
+    <Weather/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Weather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Weather() {
+    return (
+        <div>
+            <h1>Meteo</h1>
+
+        </div>
+    )
+}
+
+export default Weather
