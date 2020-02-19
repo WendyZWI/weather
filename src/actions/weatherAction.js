edf5a7492014f633b6f9e2540379ce7d11ff7cbd
WendyZW <ivy.valentin@hotmail.fr>
2020-02-19 16:48:48 +0100

    feat weather

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -6,8 +6,19 @@ import axios from 'axios';
 
 const baseUrl = "https://api.openweathermap.org/data/2.5";
 const appId = "&appid=0aaf8a1cd595c5a845064ea52996cb45";
-// const appId= "&appid=e14154b4f09fc841a46c2fd919707747";
 
-export function getWeather(city){
+//code pierre
+//const appId= "&appid=e14154b4f09fc841a46c2fd919707747";
+
+export function getWeather(){
+    return axios.get(baseUrl+"/weather?q=London"+appId);
+}
+
+export function getWeatherByCity(city){
     return axios.get(baseUrl+"/weather?q="+city+appId);
+}
+
+//Requete ajax get current weather by longitude et latitude
+export function getWeatherByCoords(coords){
+    return axios.get(baseUrl+"/weather?lat="+coords.latitude+"&lon="+coords.longitude+appId);
 }
